e72926e869eaac22dd35629af069cd79e8a0ebfb
akclifto <akclifto@asu.edu>
2021-05-21 09:21:16 -0700

    packages

---
+++ src/packages/app.py
@@ -0,0 +1,13 @@
+# brute import of school package and calc_fee module
+# import school_package.calc_fee
+# school_package.calc_fee.calc_fee()
+
+# Better to import with "from" statement
+from school_package.calc_fee import calc_fee
+# to import entire module:
+# from school_package import calc_fee
+
+calc_fee()
+calc_fee()
+calc_fee()
+
---
+++ src/packages/school_package/calc_fee.py
@@ -0,0 +1,3 @@
+def calc_fee():
+    print("calc_fee")
+
